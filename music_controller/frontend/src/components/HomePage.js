71eb4e84f421b1dfe7417745192b07718891978e
nkubana0 <i.shema@alustudent.com>
2024-03-24 20:35:15 +0200

    React components & post requests

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<p>This is the home page</p>} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
